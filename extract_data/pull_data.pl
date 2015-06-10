176d3fd1b3541a8e615bb8475658f1cab725e8e1
nanoscopic <git@codechild.com>
2015-06-10 10:58:00 -0400

    Read DB configuration from config file. Allow multiple named config files. Properly reference perl lib directories. Use config file to determine which views to dump.

--- extract_data/pull_data.pl
+++ extract_data/pull_data.pl
@@ -1,23 +1,36 @@
 #!/usr/bin/perl -w
 use strict;
-use lib '.';
-use TestPSQL;
+use lib '../../perl-Pg-Helper/lib';
+use Pg::Helper;
 use Data::Dumper;
 use XML::Bare qw/xval forcearray/;
 
-my $hostip = "127.0.0.1";
-my $user = "user";
-my $pw = "password";
+my $report_type = $ARGV[0];
+my $report_id = $ARGV[1];
 
-my $sql = TestPSQL->new( { host => $hostip, user => $user, password => $pw } );
+my ( $ob, $conf ) = XML::Bare->new( file => "../configuration/config_$report_type.xml" );
+$conf = $conf->{'xml'};
+my $pconf = $conf->{'pulldata'};
+my $dbconf = $pconf->{'db'};
 
-my $report_id = $ARGV[0];
+my $hostip = xval( $dbconf->{'ip'} );# "127.0.0.1";
+my $user = xval( $dbconf->{'user'} );#"user";
+my $pw = xval( $dbconf->{'pw'} );#"password";
+
+print "Connecting to $hostip with user $user\n";
+my $sql = Pg::Helper->new( { host => $hostip, user => $user, password => $pw } );
 
 my $xml;
 
-dumpf( 'reporting', 'fn_detail', $report_id );
+my $sources = forcearray( $pconf->{'source'} );
+for my $source ( @$sources ) {
+  my $ns = xval $source->{'ns'};
+  my $name = xval $source->{'name'};
+  dumpf( $ns, $name, $report_id );
+  #dumpf( 'reporting', 'fn_detail', $report_id );
+}
 
-open( my $f, ">raw_$report_id.xml" );
+open( my $f, ">../data/raw_${report_type}_$report_id.xml" );
 print $f XML::Bare::Object::xml( 0, $xml );
 close( $f );
 
